5fcf072bf2f53ef3ec8caea9588b580c95934d7c
Sheruq <leniv.tech@gmail.com>
2025-05-24 13:46:29 +0300

    Початковий коміт To-Do API з нового акаунта

---
+++ reminder-service/src/test/java/planer/reminder_service/ReminderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package planer.reminder_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReminderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
